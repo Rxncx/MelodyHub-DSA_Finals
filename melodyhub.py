efcafa810fd20a682f1260d234643c932d0c2fba
Lans-GB <136078950+Lans-GB@users.noreply.github.com>
2025-04-13 15:21:45 +0800

    Update melodyhub.py
    Signed-off-by: Lans-GB <136078950+Lans-GB@users.noreply.github.com>

--- melodyhub.py
+++ melodyhub.py
@@ -1,1 +1,10 @@
+songs = []
 
+while True:
+    print("\nWelcome to MelodyHub!")
+    print("[1] Add Music")
+    print("[2] View Stored Music")
+    print("[3] Generate Playlist")
+    print("[4] Exit")
+
+    choice = input("Choose an option (1-4): ")
